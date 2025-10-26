10b92d060e15b06d99ec482894709091ae44a71e
akhilNarayanoju <akhilnarayanoju1@gmail.com>
2025-10-26 11:33:39 +0530

    Clear geminiApiKey in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  geminiApiKey: 'AIzaSyAZzik3qvLMIJNtEKxxV4-a9DoyKmNT', // Replace with your actual API key from https://aistudio.google.com/app/apikey
+  geminiApiKey: '', // Replace with your actual API key from https://aistudio.google.com/app/apikey
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'
 };
